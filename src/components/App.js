9a6d49f3d6e1871fc1f291ceeecb1bdc2bb15973
Philip <pch4623@gmail.com>
2022-02-02 12:57:59 +0900

    App 컴포넌트 functional component로 리팩토링

--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
 
-class App extends React.Component {
-  state = { videos: [], selectedVideo: null };
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
-  // 컴포넌트 마운팅 직후 디폴트로 검색
-  componentDidMount() {
-    this.onTermSubmit('프로미스나인');
-  }
-
+  useEffect(() => {
+    onTermSubmit('프로미스나인');
+  }, []);  // 최초 렌더링 시 동작 (componentDidMount -> useEffect(() => {...}, []) 리팩토링)
+ 
   // SearchBar에게 props로 이벤트 핸들러 전달
-  onTermSubmit = async term => {
+  const onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
       params: {
         q : term,
       }
     });
     
-    this.setState({ 
-      videos: response.data.items,
-      selectedVideo: response.data.items[0]  // 검색했을 때 디폴트로 첫번째 비디오 셋팅
-    });
+    setVideos(response.data.items);
+    setSelectedVideo(response.data.items[0]);  // 검색했을 때 디폴트로 첫번째 비디오 셋팅
   }
 
   // VideoList -> VideoItem 까지 props 통해서 전달
-  onVideoSelect = video => {
-    this.setState({ selectedVideo: video });
+  const onVideoSelect = (video) => {
+    setSelectedVideo(video);
   }
 
-  render() {
-    return (
-      <div className='ui container'>
-        <SearchBar onFormSubmit={this.onTermSubmit} />
-        <div className='ui grid'>
-          <div className='ui row'>
-            <div className='eleven wide column'>
-              <VideoDetail video={this.state.selectedVideo} />
-            </div>
-            <div className='five wide column'>
-              <VideoList 
-                onVideoSelect={this.onVideoSelect}
-                videos={this.state.videos} 
-              />
-            </div>
+  return (
+    <div className='ui container'>
+      <SearchBar onFormSubmit={onTermSubmit} />
+      <div className='ui grid'>
+        <div className='ui row'>
+          <div className='eleven wide column'>
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className='five wide column'>
+            <VideoList 
+              onVideoSelect={onVideoSelect}
+              videos={videos} 
+            />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 
 export default App;
